405895db697ab1c2a822fb5f4eadff48209c9c08
Miessok Eric Cedric Junior <miessokeric@gmail.com>
2023-08-15 19:38:20 +0100

    CRUD Todo

--- backend/app/api/api_v1/handlers/todo.py
+++ backend/app/api/api_v1/handlers/todo.py
@@ -1,15 +1,17 @@
+from uuid import UUID
+from typing import List
 from fastapi import APIRouter, Depends
 
 from app.api.deps.user_deps import get_current_user
 from app.models.todo_model import Todo
 from app.models.user_model import User
-from app.schemas.todo_schema import TodoOut, TodoCreate
+from app.schemas.todo_schema import TodoOut, TodoCreate, TodoUpdate
 from app.services.todo_service import TodoService
 
 todo_router = APIRouter()
 
 
-@todo_router.get('/', summary="Get all todo of the user", response_model=TodoOut)
+@todo_router.get('/', summary="Get all todo of the user", response_model=List[TodoOut])
 async def list_todos(current_user: User = Depends(get_current_user)):
     return await TodoService.list_todos(current_user)
 
@@ -17,3 +19,19 @@ async def list_todos(current_user: User = Depends(get_current_user)):
 @todo_router.post('/create', summary="Create Todo", response_model=Todo)
 async def create_todo(data: TodoCreate, current_user: User = Depends(get_current_user)):
     return await TodoService.create_todo(data, current_user)
+
+
+@todo_router.get('/{todo_id}', summary="Get a Todo by id")
+async def retrieve(todo_id: UUID, current_user: User = Depends(get_current_user)):
+    return await TodoService.retrieve_todo(current_user, todo_id)
+
+
+@todo_router.put('/{todo_id}', summary="Update a Todo by todo_id")
+async def update(todo_id:  UUID, data: TodoUpdate, current_user: User = Depends(get_current_user)):
+    return await TodoService.update_todo(current_user, todo_id, data)
+
+
+@todo_router.delete('/{todo_id}', summary="Delete a todo by todo_id")
+async def delete(todo_id: UUID, current_user: User = Depends(get_current_user)):
+    await TodoService.delete_todo(current_user, todo_id)
+    return None
--- backend/app/services/todo_service.py
+++ backend/app/services/todo_service.py
@@ -1,18 +1,38 @@
 from datetime import datetime
 from typing import List
+from uuid import UUID
 
 from app.models.todo_model import Todo
 from app.models.user_model import User
-from app.schemas.todo_schema import TodoCreate
+from app.schemas.todo_schema import TodoCreate, TodoUpdate
 
 
 class TodoService:
     @staticmethod
     async def list_todos(user: User) -> List[Todo]:
-        todos = await Todo.find(Todo.owner.id == user.user_id).to_list()
+        todos = await Todo.find(Todo.owner.id == user.id).to_list()
         return todos
 
     @staticmethod
     async def create_todo(data: TodoCreate, user: User) -> Todo:
         todo = Todo(**data.dict(), owner=user, created_at=datetime.utcnow(), updated_at=datetime.utcnow())
         return await todo.create()
+
+    @staticmethod
+    async def retrieve_todo(current_user: User, todo_id: UUID):
+        todo = await Todo.find_one(Todo.todo_id == todo_id, Todo.owner.id == current_user.id)
+        return todo
+
+    @staticmethod
+    async def update_todo(current_user: User, todo_id: UUID, data: TodoUpdate):
+        todo = await TodoService.retrieve_todo(current_user, todo_id)
+        await todo.update({"$set": data.dict(exclude_unset=True)})
+        await todo.save()
+        return todo
+
+    @staticmethod
+    async def delete_todo(current_user: User, todo_id: UUID):
+        todo = await TodoService.retrieve_todo(current_user, todo_id)
+        if todo:
+            await todo.delete()
+        return None
